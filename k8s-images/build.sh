a0e02a302b1d23d302f96663275474ddf67b168d
reachmohancareers88 <reachmohancareers@gmail.com>
2026-06-15 12:15:18 +0530

    Update build.sh

--- k8s-images/build.sh
+++ k8s-images/build.sh
@@ -1,3 +1,14 @@
-if [ -z "$1" ] ; then echo Input Dockerfile ; exit 1 ;fi
-docker build -f $1 -t rkalluru/$1 .
-docker push rkalluru/$1
+if [ -z "$1" ] ; then
+  echo "Input Dockerfile"
+  exit 1
+fi
+
+az acr login --name mohanacr89
+
+docker build -f $1 -t mohanacr89.azurecr.io/$1 .
+docker push mohanacr89.azurecr.io/$1
+
+##if [ -z "$1" ] ; then echo Input Dockerfile ; exit 1 ;fi
+##docker build -f $1 -t mohanacr89.azurecr.io/$1 .
+##docker push mohanacr89.azurecr.io/$1
+~
